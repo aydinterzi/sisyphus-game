bf6f7c99fdb9025c4f4659e49cac9770b5f5f3f7
Aydin Terzi <aydinterzi7@gmail.com>
2025-04-17 21:23:06 +0300

    scene

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { BoxGeometry, MeshBasicMaterial } from "three";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Canvas
+      camera={{
+        position: [0, 10, 20],
+        fov: 60,
+      }}
+    >
+      <OrbitControls />
+      <mesh
+        geometry={new BoxGeometry(1, 1, 1)}
+        material={new MeshBasicMaterial()}
+      />
+    </Canvas>
+  );
 }
 
-export default App
+export default App;
